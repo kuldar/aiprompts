c0d17afcb2e9f8f1e0bae4a70dd40447250746a7
Kuldar <hi@kuldar.com>
2022-09-07 14:41:59 +0300

    umm

--- prisma/migrations/20220907114136_/migration.sql
+++ prisma/migrations/20220907114136_/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -25,6 +26,9 @@ CREATE TABLE "Note" (
 
     CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
