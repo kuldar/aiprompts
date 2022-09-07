fae1d500fcd7f030902c41255aef989656e43281
Kuldar <hi@kuldar.com>
2022-09-07 14:15:16 +0300

    hmmm

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,18 +5,21 @@ const bcrypt = require("bcryptjs");
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "bob@example.com";
+  const username = "bob";
+  const password = "password";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await prisma.user.create({
     data: {
       email,
+      username,
       password: {
         create: {
           hash: hashedPassword,
