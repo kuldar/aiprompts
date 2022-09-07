b77954ab7d37599e3c4d01fe99930710963c0887
Kuldar <hi@kuldar.com>
2022-09-07 11:52:09 +0300

    change deployment target

--- app/routes/healthcheck.jsx
+++ app/routes/healthcheck.jsx
@@ -1,13 +1,8 @@
-// learn more: https://fly.io/docs/reference/configuration/#services-http_checks
-
 import { prisma } from "~/db.server";
 
 export async function loader({ request }) {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
-
   try {
-    const url = new URL("/", `http://${host}`);
+    const url = new URL(`http://localhost:${process.env.PORT ?? 3000}/`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
